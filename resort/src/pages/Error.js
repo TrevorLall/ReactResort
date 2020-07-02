44629ac7a1855eca3ac4c4e829e5e9057d49b091
TrevorLall <tlall0308@gmail.com>
2020-07-02 12:44:24 -0400

    Added New Elements

--- resort/src/pages/Error.js
+++ resort/src/pages/Error.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Hero from "../components/Hero"
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 
 export default function Error() {
     return (
-        <div>
-            Hello from Error
-        </div>
+        <Hero>
+            <Banner title="404"
+                subtitle="Page not found">
+                <Link to='/' className="btn-primary">Home Page</Link>
+            </Banner>
+        </Hero>
     )
 }
--- resort/src/pages/Rooms.js
+++ resort/src/pages/Rooms.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 export default function Rooms() {
     return (
-        <div>
-            Hello From Rooms Page
-        </div>
+        <Hero hero="roomsHero">
+            <Banner title="Our Rooms"
+                subtitle="">
+                <Link to='/' className="btn-primary">Return Home</Link>
+            </Banner>
+        </Hero>
     )
 }

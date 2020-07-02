f684d31edc21c80719cad0bcd2ed7a6798f54ef1
TrevorLall <tlall0308@gmail.com>
2020-07-02 11:49:12 -0400

    Init

---
+++ resort/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import logo from '../images/logo.svg'
+import { FaAlignRight } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                Hello From Navbar
+            </div>
+        )
+    }
+}

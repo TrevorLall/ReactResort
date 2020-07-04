2966f5c7396589187b9c0e727428d8fabe099008
TrevorLall <tlall0308@gmail.com>
2020-07-03 22:20:37 -0400

    Added Services

--- resort/src/components/Services.js
+++ resort/src/components/Services.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Title from './Title'
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer, FaUtensilSpoon } from 'react-icons/fa'
 
-export default function Services() {
-    return (
-        <div>
+export default class Services extends Component {
+    state = {
+        services: [
+            {
+                icon: <FaCocktail />,
+                title: "Free Cocktails",
+                info: "This is random text for various services offered by the random site"
+            },
+            {
+                icon: <FaHiking />,
+                title: "Endless Hiking",
+                info: "This is random text for various services offered by the random site"
+            },
+            {
+                icon: <FaShuttleVan />,
+                title: "Free Shuttle",
+                info: "This is random text for various services offered by the random site"
+            },
+            {
+                icon: <FaBeer />,
+                title: "Cervezas For Days",
+                info: "This is random text for various services offered by the random site"
+            }
+        ]
+    }
+    render() {
+        return (
+            <section className="services">
+                <Title title='services'></Title>
+                <div className="services-center">
+                    {this.state.services.map((item, index) => {
+                        return <article key={index} className="service">
+                            <span>{item.icon}</span>
+                            <h6>{item.title}</h6>
+                            <p>{item.info}</p>
+                        </article>
+                    })}
+                </div>
 
-        </div>
-    )
+            </section>
+        )
+    }
 }
+
+

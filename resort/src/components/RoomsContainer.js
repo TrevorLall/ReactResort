27d0ad0636de3a8a8ae9ccf8184cbe5a42e19fce
TrevorLall <tlall0308@gmail.com>
2020-07-06 13:56:49 -0400

    Final Resort Code, Added filter

---
+++ resort/src/components/RoomsContainer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import RoomsFilter from './RoomsFilter'
+import RoomsList from './RoomsList'
+import { RoomConsumer } from '../context'
+import Loading from '../components/Loading'
+
+export default function RoomsContainer() {
+
+    return (
+        <RoomConsumer>
+            {
+                value => {
+                    const { loading, sortedRooms, rooms } = value;
+                    if (loading) {
+                        return <Loading></Loading>;
+                    }
+                    return (
+                        <div>
+                            <RoomsFilter rooms={rooms}></RoomsFilter >
+                            <RoomsList rooms={sortedRooms}></RoomsList>
+                        </div>
+                    )
+                }
+            }
+        </RoomConsumer>
+    )
+}
